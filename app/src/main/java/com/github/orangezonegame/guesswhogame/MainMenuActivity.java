d5e8242194176d08b31561f1b401f73d9bbf8d54
MGibran <maikgibranr@gmail.com>
2018-01-01 00:57:32 +0700

    Initial commit

---
+++ app/src/main/java/com/github/orangezonegame/guesswhogame/MainMenuActivity.java
@@ -0,0 +1,28 @@
+package com.github.orangezonegame.guesswhogame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import info.hoang8f.widget.FButton;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_room_code)
+    EditText etRoomCode;
+
+    @BindView(R.id.button_host)
+    FButton buttonHost;
+
+    @BindView(R.id.button_join)
+    FButton buttonJoin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        ButterKnife.bind(this);
+    }
+}
